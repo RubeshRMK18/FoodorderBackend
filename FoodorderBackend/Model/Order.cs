ec26724aa48a3a24203f112a32c99aab4f7bbf5d
MNithishKanna12345 <mnithipecit01@gmail.com>
2026-04-10 12:57:17 +0530

    Order module

---
+++ FoodorderBackend/Model/Order.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodorderBackend.Model
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        public string FoodName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
