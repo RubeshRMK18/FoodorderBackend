22a6198028921dbd9d816d5e27c5a2a58a985d86
harisakthi301104 <harisakthi301104@gmail.com>
2026-04-10 10:59:53 +0530

    commiting dto

---
+++ FoodorderBackend/Dto/FoodCreateDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodOrderingSystem.Dto
+{
+    public class FoodCreateDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
